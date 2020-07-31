3b15b6b5172646c3961f26022b1675d092cb5be9
ELCANET\mmg <mirza.musharaf.golapkhan@elca.mu>
2020-07-31 14:29:08 +0400

    frg

---
+++ projet-layout-service/src/main/java/service/model/ResourceDto.java
@@ -0,0 +1,22 @@
+package service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class ResourceDto {
+
+    String status;
+    String visa;
+    String firstName;
+    String lastName;
+
+}
